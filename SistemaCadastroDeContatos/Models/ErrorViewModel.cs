ed6bb78fa3ba2055af396719375dd3f33f1e5fb5
Ruan92929 <ruan92929@hotmail.com>
2023-09-27 20:44:54 -0300

    Implementando Front-end

---
+++ SistemaCadastroDeContatos/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SistemaCadastroDeContatos.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
